dfb83bbc647ce9d630d7950918aa185abe8ff45f
andrei-mac <vasy1796@gmail.com>
2021-09-01 22:29:52 +0100

    backup

--- array_factor_av.m
+++ array_factor_av.m
@@ -1,4 +1,5 @@
 clear all;
+%load('./project_scripts/conv_alpha_d.mat')
 
 % imaginary 
 j=sqrt(-1);
@@ -12,23 +13,23 @@ k=2*pi/lambda;
 % number of elements
 N=30;
 % phase shift of the elements in degrees
-alpha=60;
+alpha=-70;
 % distancing limit from project spec
 d_limit = 0.6*lambda;
 % x axis spec
 theta=0:0.02:pi;
 
 % simulation for different spacings
-for i=0:0.001:d_limit
+for i=0:0.001:d_limit/2
     % uniform element spacing experiment
     % d = i;
     % reinitialise AF on each run
     AF_init = zeros(30,length(theta));
     for m=1:N
         % individual spaced elements experiment
-        d = (d_limit-0).*rand(1,1) + 0;
+        % I = (1.0-0).*rand(1,1) + 0;
         % from UOT lecture
-        AF_init(m,:) = 1*exp(j*k*(m-1)*d*cos(theta)+j*(m-1)*(alpha*pi)/180);
+        AF_init(m,:) = 1*exp(j*k*(m-1)*d*cos(theta)+j*(m-1)*alpha*pi/180);
     end
     AF_sum = sum(AF_init(1:N,:));
     AF_abs = abs(AF_sum);
@@ -41,13 +42,13 @@ for i=0:0.001:d_limit
     plot(theta,AF);
     hold on;
     % constant line for SLL threshold
-    yline(-10,'r');
+    % yline(-10,'r');
     hold off;
     title(sprintf('Log plot with spacing of %s', d));
     xlabel('Angle [rad]');
     ylabel('AF [dB]');
     grid;
-    legend('Radiation pattern','SLL limit');
+    legend('Radiation pattern');
     
     nexttile
     % polar radiation pattern 
